1ca0fb72fa776c5d575e663328982acc4062b51a
Amilcar Menjivar <amilcar.menjivar11@gmail.com>
2022-09-24 13:17:32 +0200

    obtener props para universidades (SSG y SSR)
    closes #1

--- pages/universidades.tsx
+++ pages/universidades.tsx
@@ -1,96 +1,29 @@
 import { University } from '../types'
 import ListadoUniversidades from '../components/universidades/ListadoUniversidades'
 import TituloUniversidades from '../components/universidades/TituloUniversidades'
+import { GetServerSideProps } from 'next'
 
-// TODO: Data fetch: API
-const listadoUniversidades: University[] = [
-  {
-    id: 1,
-    slug: 'ie-universidad',
-    name: 'IE Universidad',
-    shortName: 'IE',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/1/ie-universidad-logo.jpg',
-  },
-  {
-    id: 2,
-    slug: 'mondragon-unibertsitatea',
-    name: 'Mondragón Unibertsitatea',
-    shortName: 'MU',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/2/mondragon-unibertsitatea-logo.jpg',
-  },
-  {
-    id: 3,
-    slug: 'otros-centros-de-nivel-universitario',
-    name: 'Otros Centros de Nivel Universitario',
-    shortName: 'OCNU',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/3/otros-centros-de-nivel-universitario-logo.jpg',
-  },
-  {
-    id: 4,
-    slug: 'universidad-a-distancia-de-madrid',
-    name: 'Universidad a Distancia de Madrid',
-    shortName: 'UDIMA',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/4/universidad-a-distancia-de-madrid-logo.jpg',
-  },
-  {
-    id: 5,
-    slug: 'universidad-alfonso-x-el-sabio',
-    name: 'Universidad Alfonso X El Sabio',
-    shortName: 'UAX',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/5/universidad-alfonso-x-el-sabio-logo.jpg',
-  },
-  {
-    id: 6,
-    slug: 'universidad-antonio-de-nebrija',
-    name: 'Universidad Antonio de Nebrija',
-    shortName: 'UAN',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/6/universidad-antonio-de-nebrija-logo.jpg',
-  },
-  {
-    id: 7,
-    slug: 'universidad-autonoma-de-barcelona',
-    name: 'Universidad Autónoma de Barcelona',
-    shortName: 'UAB',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/7/universidad-autonoma-de-barcelona-logo.jpg',
-  },
-  {
-    id: 8,
-    slug: 'universidad-autonoma-de-madrid',
-    name: 'Universidad Autónoma de Madrid',
-    shortName: 'UAM',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/8/universidad-autonoma-de-madrid-logo.jpg',
-  },
-  {
-    id: 9,
-    slug: 'universidad-camilo-jose-cela',
-    name: 'Universidad Camilo José Cela',
-    shortName: 'UCJC',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/9/universidad-camilo-jose-cela-logo.jpg',
-  },
-  {
-    id: 10,
-    slug: 'universidad-cardenal-herrera-ceu',
-    name: 'Universidad CEU Cardenal Herrera',
-    shortName: 'UCHCEU',
-    logoUrl:
-      'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/10/universidad-cardenal-herrera-ceu-logo.jpg',
-  },
-]
+type Props = {
+  listadoUniversidades: University[]
+}
 
-export default function Universidades() {
+export default function Universidades({ listadoUniversidades }: Props) {
   return (
     <>
       <TituloUniversidades />
       <ListadoUniversidades lista={listadoUniversidades} />
     </>
   )
+}
+
+// issue #1: SSR listado de universidades, según propiedades del request
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const data = await getUnivesitiesByPage(0)
+  return { props: { listadoUniversidades: data.data } }
+}
+
+async function getUnivesitiesByPage(page: number) {
+  // TODO: pagination
+  return await fetch(`https://api.wuolah.com/v2/universities`).then(res => res.json())
+  // TODO: try-catch
 }
--- pages/universidades/[slugUniversidad].tsx
+++ pages/universidades/[slugUniversidad].tsx
@@ -1,19 +1,17 @@
-import Link from 'next/link'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { University } from '../../types'
 
-// TODO: fetch data from API
-const defaultProps = {
-  id: 2,
-  slug: 'mondragon-unibertsitatea',
-  name: 'Mondragón Unibertsitatea',
-  shortName: 'MU',
-  logoUrl:
-    'https://s3-eu-west-1.amazonaws.com/wuolah-public/imagenes/universidades/2/mondragon-unibertsitatea-logo.jpg',
+type Props = {
+  data: University
 }
 
-const data = { ...defaultProps }
+// TODO: crear componente visualmente agradable.
 
-function PaginaUniversidad() {
+function PaginaUniversidad({ data }: Props) {
+  if (!data) return <h1>No data found</h1>
+
   return (
     <>
       <Head>
@@ -28,13 +26,52 @@ function PaginaUniversidad() {
         />
       </Head>
 
-      <meta property="og:title" content="My page title" key="title" />
-
       <Link href="/universidades">Go Back</Link>
 
       <h3>{data.name}</h3>
+      <h4>{data.shortName}</h4>
     </>
   )
+}
+
+// issue #1: SSG universidades top 20% más populares; SSR las demás (via fallback)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const data = await getUnivesityProps(params?.slugUniversidad as string)
+
+  return { props: { data } }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const topUnivesities = await getTopUnivesities()
+  const paths = topUnivesities.map(u => ({ params: { slugUniversidad: u.slug } }))
+
+  return {
+    paths,
+    // Creo que blocking es mejor para SEO, pero hay que confirmarlo
+    fallback: true,
+  }
+}
+
+async function getTopUnivesities(): Promise<University[]> {
+  // TODO: consumir EP de universidades más populares.
+  // por el momento se utilizan las primeras de la lista.
+  try {
+    return await fetch(`https://api.wuolah.com/v2/universities`)
+      .then(res => res.json())
+      .then(data => data.data)
+  } catch (error) {
+    console.error('Problema al obtener universidades populares.', error)
+    return []
+  }
+}
+
+async function getUnivesityProps(slug: string) {
+  try {
+    return await fetch(`https://api.wuolah.com/v2/universities/${slug}`).then(res => res.json())
+  } catch (error) {
+    console.error('Problema al obtener props de universidad: ' + slug, error)
+    return null
+  }
 }
 
 export default PaginaUniversidad
